5e1df2d75d57b36b064dd0511d000dabc632725d
nonoBubu <arnaud.burban@gmail.com>
2019-10-07 22:54:29 +0200

    test connection

--- src/main/java/achille/AchilleApplication.java
+++ src/main/java/achille/AchilleApplication.java
@@ -1,12 +1,18 @@
 package achille;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AchilleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
+		InetAddress ip;
+		ip = InetAddress.getLocalHost();
+		System.out.println("Current IP address : " + ip.getHostAddress());
 		SpringApplication.run(AchilleApplication.class, args);
 	}
 }
